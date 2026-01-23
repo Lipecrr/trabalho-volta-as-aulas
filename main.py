0a994346e41a1f39cd2b04c2fe4653e24784d98f
Felipe Fernando Correa <lipeaba13@gmail.com>
2026-01-23 19:32:57 -0300

    Criado endpoits para Aluno

--- main.py
+++ main.py
@@ -55,3 +55,34 @@ def cadastrar_aluno(aluno: AlunoCriar, db: Session = Depends(get_db)):
         aluno.data_nascimento
     )
     return aluno
+
+
+@app.get("/api/v1/alunos", tags=["Alunos"])
+def listar_cliente(db: Session = Depends(get_db)):
+    alunos = fjvcursos_aluno_repositorio.obter_todos(db)
+    return alunos
+
+
+@app.delete("/api/v1/alunos/{id}", tags=["Alunos"])
+def apagar_aluno(id: int, db: Session = Depends(get_db)):
+    linhas_afetadas = fjvcursos_aluno_repositorio.apagar(db, id)
+    if not linhas_afetadas:
+        raise HTTPException(status_code=404, detail="Aluno não encontrado")
+    return {"status": "ok"}
+
+
+@app.put("/api/v1/alunos/{id}", tags=["Alunos"])
+def editar_aluno(id: int, aluno: AlunoEditar, db: Session = Depends(get_db)):
+    linhas_afetadas = fjvcursos_aluno_repositorio.editar(
+        db, id,  aluno.nome, aluno.telefone,aluno.data_nascimento,
+    )
+    if not linhas_afetadas:
+        raise HTTPException(status_code=404, detail="Aluno não encontrado")
+    return {"status": "ok"}
+
+
+@app.get("/api/v1/alunos/{id}", tags=["Alunos"])
+def listar_aluno(id: int, db: Session = Depends(get_db)):
+    aluno = fjvcursos_aluno_repositorio.obter_por_id(db, id)
+    if not aluno:
+        raise HTTPException(status_code=404, detail="Aluno não encontrado")
